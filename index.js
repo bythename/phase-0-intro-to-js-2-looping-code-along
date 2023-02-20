1769b214012fad79e664e461e53845dc65ed30bc
Eun Jeong <gracejeong767@gmail.com>
2023-02-19 16:16:15 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names) {
+    let newNames = []
+    for (let i = 0; i <names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return newNames;
+}
+console.log(names);
+
+function countDown() {
+    let i = 10
+    while (i>=0) {
+        console.log(i);
+        i--;
+    }
+}
+console.log(countDown(i));
